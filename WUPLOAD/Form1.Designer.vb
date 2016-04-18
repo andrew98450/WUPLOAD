00988d1edc705177325deb95f949904d6d5549e7
andrew98450 <ktpss96034@yahoo.com.tw>
2016-04-18 21:10:02 +0800

    WUPLOAD Code

---
+++ WUPLOAD/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Target URL:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 12)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Upload File:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(82, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(552, 22)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(82, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(552, 22)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(649, 18)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(79, 25)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "UPFILE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(649, 61)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(79, 25)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Path"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Enabled = False
+        Me.RichTextBox1.Location = New System.Drawing.Point(16, 143)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical
+        Me.RichTextBox1.Size = New System.Drawing.Size(714, 104)
+        Me.RichTextBox1.TabIndex = 7
+        Me.RichTextBox1.Text = ""
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(14, 375)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(353, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Save Log"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(373, 375)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(355, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Clear Log"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("新細明體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label4.Location = New System.Drawing.Point(681, 299)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(33, 19)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "0%"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 299)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(648, 23)
+        Me.ProgressBar1.TabIndex = 15
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 342)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(75, 12)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "Save Log Path:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(93, 339)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(541, 22)
+        Me.TextBox3.TabIndex = 17
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(649, 336)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(79, 25)
+        Me.Button5.TabIndex = 18
+        Me.Button5.Text = "Path"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(258, 105)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(45, 16)
+        Me.RadioButton1.TabIndex = 19
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "GET"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 107)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(81, 12)
+        Me.Label6.TabIndex = 20
+        Me.Label6.Text = "Upload Method:"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(427, 105)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(50, 16)
+        Me.RadioButton2.TabIndex = 21
+        Me.RadioButton2.Text = "POST"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 268)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(138, 12)
+        Me.Label3.TabIndex = 22
+        Me.Label3.Text = "Upload Status: 0 MB / 0 MB"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(740, 413)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "WUPLOAD"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ WUPLOAD/Form1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Net
+Public Class Form1
+    Dim WithEvents web As New WebClient
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If RadioButton1.Checked = True Then
+            Try
+                RichTextBox1.Text += "GET Upload Time: " + Date.Now.ToString + vbCrLf
+                RichTextBox1.Text += "GET Method : " + TextBox2.Text + " To " + TextBox1.Text + " Uploading..."
+                web.UploadFileAsync(New Uri(TextBox1.Text), "GET", TextBox2.Text)
+            Catch ex As Exception
+                RichTextBox1.Text += "GET Upload Fali..."
+            End Try
+        ElseIf RadioButton2.Checked = True Then
+            Try
+                RichTextBox1.Text += "POST Upload Time: " + Date.Now.ToString + vbCrLf
+                RichTextBox1.Text += "POST Method : " + TextBox2.Text + " To " + TextBox1.Text + " Uploading..."
+                web.UploadFileAsync(New Uri(TextBox1.Text), "POST", TextBox2.Text)
+            Catch ex As Exception
+                RichTextBox1.Text += "POST Upload Fali..."
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        OpenFileDialog1.ShowDialog()
+        TextBox2.Text = OpenFileDialog1.FileName
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox3.Text = My.Computer.FileSystem.SpecialDirectories.Desktop + "\Log.txt"
+        SaveFileDialog1.Filter = "*.txt|*.txt|*.all|*.*"
+        OpenFileDialog1.Filter = "*.all|*.*"
+    End Sub
+    Private Sub web_UploadFileCompleted(sender As Object, e As UploadFileCompletedEventArgs) Handles web.UploadFileCompleted
+    End Sub
+    Private Sub web_UploadProgressChanged(sender As Object, e As UploadProgressChangedEventArgs) Handles web.UploadProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+        Label3.Text = "Upload Status: " & e.BytesSent.ToString / 1000000 & " MB / " & e.TotalBytesToSend.ToString / 1000000 & " MB" + vbCrLf
+        Label4.Text = ProgressBar1.Value & "%"
+        If ProgressBar1.Value = 100 Then
+            If RadioButton1.Checked = True Then
+                RichTextBox1.Text += "GET Upload Successfly..." + vbCrLf
+            ElseIf RadioButton2.Checked = True Then
+                RichTextBox1.Text += "POST Upload Successfly..." + vbCrLf
+            End If
+        End If
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        SaveFileDialog1.ShowDialog()
+        TextBox3.Text = SaveFileDialog1.FileName
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        RichTextBox1.Text = ""
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        My.Computer.FileSystem.WriteAllText(TextBox3.Text, RichTextBox1.Text, True)
+        MessageBox.Show("Successfly Save Log File Path: " + TextBox3.Text)
+    End Sub
+End Class
